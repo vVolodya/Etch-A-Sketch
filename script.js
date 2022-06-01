450e3929bd1cf6d6b5ad2b5dfe5312140f4a271b
vVolodya <skinner.vova@gmail.com>
2022-06-01 21:27:43 +1000

    It's working, UI after

---
+++ script.js
@@ -0,0 +1,69 @@
+createBoard(16);
+
+document.querySelector('body').addEventListener('click', (e) => {
+	if(e.target.tagName != 'BUTTON') {
+		click = !click
+		if(click) {
+			document.querySelector('.mode').textContent = 'Mode: Coloring';
+		} else {
+			document.querySelector('.mode').textContent = 'Mode: Not Coloring';
+		}
+	}
+});
+
+let color = 'black';
+let click = true;
+
+function createBoard (size) {
+
+	let board = document.querySelector('.board');
+	let squares = board.querySelectorAll('div');
+	squares.forEach((div) => div.remove());
+	board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+	for (let i = 0; i < Math.pow(size, 2); i++) {
+	let square = document.createElement('div');
+	square.addEventListener('mouseover', colorSquare);
+	square.style.backgroundColor = 'white';
+	board.appendChild(square);
+	}
+
+}
+
+function changeSize (input) {
+	if(input >= 2 && input <= 64) {
+		createBoard(input);
+		removeInputError();
+	} else {
+		addInputError();
+	}
+}
+
+function colorSquare() {
+	if (click) {
+		if(color === 'random') {
+			this.style.backgroundColor = `hsl(${Math.random()*360}, 100%, 50%)`;
+		} else {
+			this.style.backgroundColor = color;
+		}
+	}
+}
+
+function changeColor(choice) {
+	color = choice;
+}
+
+function resetBoard() {
+	let board = document.querySelector('.board');
+	let squares = board.querySelectorAll('div');
+	squares.forEach((div) => div.style.backgroundColor = 'white');
+}
+
+function addInputError() {
+	document.querySelector('.error').style.display = 'flex';
+}
+
+function removeInputError() {
+	document.querySelector('.error').style.display = 'none';
+}
